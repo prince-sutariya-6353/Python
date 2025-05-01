ba98ea67df73756e8acb9bfc2322a6cbce1a9ce1
prince <princegithub6353@gmail.com>
2025-05-01 14:37:23 +0530

    Update Rename a File.py

--- Rename a File.py
+++ Rename a File.py
@@ -1,10 +1,10 @@
 import os
 
 # Current file name with path
-old_name = r'E:\Internship\Python\pro\data1.txt'
+old_name = r'E:\Internship\Python\pro\data.txt'
 
 # New file name with path
-new_name = r'E:\Internship\Python\pro\data.txt'
+new_name = r'E:\Internship\Python\pro\data1.txt'
 
 # Rename the file
 os.rename(old_name, new_name)
